f6e0c3bcedad1039d596f71a7c7a20983526b91e
Stark-program <chad.d.stark@hotmail.com>
2023-02-06 19:06:40 -0800

    work on implementing website animations

--- components/framer-motion/Name.tsx
+++ components/framer-motion/Name.tsx
@@ -1,21 +1,52 @@
-import { motion } from "framer-motion";
+import { motion, VariantLabels, useAnimationControls } from "framer-motion";
+import { useState } from "react";
 
 const Name = (props: Props) => {
+  const [hover, setHover] = useState(false);
+
   const name = props.name.split("");
+  const controls = useAnimationControls();
+
+  const rubberTransition = () => {
+    controls.start({
+      transform: [
+        "scale3d(1,1,1)",
+        "scale3d(1.4,.55,1)",
+        "scale3d(.75,1.25,1)",
+        "scale3d(1.25,.85,1)",
+        "scale3d(.9,1.05,1)",
+        "scale3d(1,1,1)",
+      ],
+      transition: {
+        times: [0, 0.4, 0.6, 0.7, 0.8, 0.9],
+      },
+    });
+    setHover(true);
+  };
+
   return (
-    <h1 className="text-[80px] font-Calistoga md:text-[130px] ">
+    <h4 className="text-[30px] font-Calistoga md:text-[40px] ">
       {name.map((letter, index) => {
+        console.log(letter);
         return (
           <motion.span
             key={index}
             className="inline-block"
-            whileHover={{ scale: 1.1 }}
+            animate={controls}
+            onMouseOver={() => {
+              if (!hover) {
+                rubberTransition();
+              }
+            }}
+            onAnimationComplete={() => {
+              setHover(false);
+            }}
           >
-            {letter}
+            {letter === " " ? "\u00A0" : letter}
           </motion.span>
         );
       })}
-    </h1>
+    </h4>
   );
 };
 
--- components/framer-motion/Title.tsx
+++ components/framer-motion/Title.tsx
@@ -1,7 +1,29 @@
-import { motion } from "framer-motion";
+import { motion, useAnimationControls } from "framer-motion";
+import { useState } from "react";
 
 const Title = (props: Props) => {
+  const controls = useAnimationControls();
+  const [hover, setHover] = useState(false);
+
   let title = props.title.split("");
+
+  const rubberTransition = () => {
+    controls.start({
+      transform: [
+        "scale3d(1,1,1)",
+        "scale3d(1.4,.55,1)",
+        "scale3d(.75,1.25,1)",
+        "scale3d(1.25,.85,1)",
+        "scale3d(.9,1.05,1)",
+        "scale3d(1,1,1)",
+      ],
+      transition: {
+        times: [0, 0.4, 0.6, 0.7, 0.8, 0.9],
+      },
+    });
+    setHover(true);
+  };
+
   return (
     <h4 className="text-[30px] font-Calistoga md:text-[40px] ">
       {title.map((letter, index) => {
@@ -9,9 +31,17 @@ const Title = (props: Props) => {
           <motion.span
             key={index}
             className="inline-block"
-            whileHover={{ scale: 1.1 }}
+            animate={controls}
+            onMouseOver={() => {
+              if (!hover) {
+                rubberTransition();
+              }
+            }}
+            onAnimationComplete={() => {
+              setHover(false);
+            }}
           >
-            {letter}
+            {letter === " " ? "\u00A0" : letter}
           </motion.span>
         );
       })}
